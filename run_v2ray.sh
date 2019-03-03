b4d83a7b0a6bf42c9100185d12a20fea3303839f
Yukiho Kikuchi <bclswl0827@foxmail.com>
2019-03-03 17:16:54 +0800

    "port":30000

--- run_v2ray.sh
+++ run_v2ray.sh
@@ -1,5 +1,5 @@
 cd /v2ray
 sed -i "s/uuid/$UUID/g" config.json
 sed -i "s/v2raypath/$V2RAYPATH/g" config.json
-echo -e '{"inbound":{"port":3050,"listen":"127.0.0.1","protocol":"vmess","settings":{"clients":[{"id":"uuid","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"v2raypath"}}},"outbound":{"protocol":"freedom","settings":{}}}' > config.json
+echo -e '{"inbound":{"port":30000,"listen":"127.0.0.1","protocol":"vmess","settings":{"clients":[{"id":"uuid","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"v2raypath"}}},"outbound":{"protocol":"freedom","settings":{}}}' > config.json
 ./v2ray
