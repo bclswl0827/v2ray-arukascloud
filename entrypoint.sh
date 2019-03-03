36ee4338dc115eee42bdd8fec8ad936820c866ef
Yukiho Kikuchi <bclswl0827@foxmail.com>
2019-03-03 16:19:04 +0800

    Shell for V2Ray

--- entrypoint.sh
+++
@@ -1,3 +0,0 @@
-echo -e '{"inbound":{"port":3050,"listen":"127.0.0.1","protocol":"vmess","settings":{"clients":[{"id":"$UUID","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"$V2RAYPATH"}}},"outbound":{"protocol":"freedom","settings":{}}}' > /v2ray/config.json
-screen -dmS v2ray /v2ray/v2ray -config=/v2ray/config.json
-screen -dmS caddy /caddy/caddy -conf=/caddy/Caddyfile
---
+++ run_v2ray.sh
@@ -0,0 +1,3 @@
+cd /v2ray
+echo -e '{"inbound":{"port":3050,"listen":"127.0.0.1","protocol":"vmess","settings":{"clients":[{"id":"$UUID","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"$V2RAYPATH"}}},"outbound":{"protocol":"freedom","settings":{}}}' > config.json
+./v2ray -config=./config.json
