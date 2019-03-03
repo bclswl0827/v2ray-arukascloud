88c01c7e24cf83126122b8003f7e4f2224c14336
Yukiho Kikuchi <bclswl0827@foxmail.com>
2019-03-03 17:07:28 +0800

    sed -i "s/uuid/$UUID/g" wwwroot/config.json

--- run_caddy.sh
+++ run_caddy.sh
@@ -1,2 +1,5 @@
 cd /caddy
-./caddy -conf=./Caddyfile
+sed -i "s/uuid/$UUID/g" wwwroot/config.json
+sed -i "s/v2raypath/$V2RAYPATH/g" wwwroot/config.json
+sed -i "s/endpoint/$ENDPOINT/g" wwwroot/config.json
+./caddy
