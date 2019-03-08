3e29d1995147b7a9bc62edcacc5fbd4e897f0ad3
Yukiho Kikuchi <bclswl0827@foxmail.com>
2019-03-08 23:46:44 +0900

    Changes

--- run.sh
+++ run.sh
@@ -1,15 +1,6 @@
 # Configure V2Ray
-cd /v2ray
-echo -e '{"inbound":{"port":30000,"listen":"127.0.0.1","protocol":"vmess","settings":{"clients":[{"id":"uuid","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"v2raypath"}}},"outbound":{"protocol":"freedom","settings":{}}}' > config.json
-sed -i "s/uuid/$UUID/g" config.json
-sed -i "s/v2raypath/$RAY/g" config.json
-# Configure Caddy
-cd /caddy
-sed -i "s/endpoint/$ENDPOINT/g" Caddyfile
-sed -i "s/v2raypath/$RAY/g" Caddyfile
-sed -i "s/uuid/$UUID/g" wwwroot/config.json
-sed -i "s/v2raypath/$RAY/g" wwwroot/config.json
-sed -i "s/endpoint/$ENDPOINT/g" wwwroot/config.json
+echo -e '{"inbound":{"port":30000,"listen":"127.0.0.1","protocol":"vmess","settings":{"clients":[{"id":"uuid","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/ws"}}},"outbound":{"protocol":"freedom","settings":{}}}' > /v2ray/config.json
+sed -i "s/uuid/$UUID/g" /v2ray/config.json
 # Run V2Ray & Caddy
-nohup /v2ray/v2ray -config=/v2ray/config.json >v2ray.out 2>&1 &
-nohup /caddy/caddy -conf=/caddy/Caddyfile >caddy.out 2>&1 &
+screen -dmS v2ray /v2ray/v2ray -config=/v2ray/config.json
+screen -dmS /caddy/caddy -conf=/caddy/Caddyfile
