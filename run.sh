510b9ebd6bc56f10d809214b70c1df50d2f006b5
Yukiho Kikuchi <bclswl0827@foxmail.com>
2019-03-09 17:06:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 # Configure V2Ray
-echo -e '{"inbound":{"port":30000,"listen":"127.0.0.1","protocol":"vmess","settings":{"clients":[{"id":"uuid","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/ws"}}},"outbound":{"protocol":"freedom","settings":{}}}' > /v2ray/config.json
+echo -e '{"inbound":{"port":80,"listen":"0.0.0.0","protocol":"vmess","settings":{"clients":[{"id":"uuid","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/ws"}}},"outbound":{"protocol":"freedom","settings":{}}}' > /v2ray/config.json
 sed -i "s/uuid/$UUID/g" /v2ray/config.json
-# Run V2Ray & Caddy
-screen -dmS v2ray /v2ray/v2ray -config=/v2ray/config.json
-screen -dmS /caddy/caddy -conf=/caddy/Caddyfile
+# Run V2Ray
+/v2ray/v2ray -config=/v2ray/config.json
